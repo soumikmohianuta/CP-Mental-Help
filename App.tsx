adb85b71b9a7ef8ab3682fa619c50a3b330fe5b2
farhadyasir <iraz2426@gmail.com>
2020-05-13 21:48:20 +1000

    feat: add navigation and styled components

--- App.tsx
+++ App.tsx
@@ -1,19 +1,17 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import { HomeScreen } from './screens/home';
+
+const { Navigator, Screen } = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Navigator>
+        <Screen name="Home" component={HomeScreen} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,1 @@
+# CP MENTAL HELP APP
